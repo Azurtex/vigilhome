284092ab624fc3622302843fa4f65e026792e337
Yann Mangiagli <ph93gae@eduvaud.ch>
2023-06-09 15:45:03 +0200

    ajout début interface

--- vigilhome/Form1.Designer.cs
+++ vigilhome/Form1.Designer.cs
@@ -31,7 +31,11 @@
             this.lblConsigne = new System.Windows.Forms.Label();
             this.txtEntry = new System.Windows.Forms.TextBox();
             this.btnValidate = new vigilhome.RJButton();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.lblTracks = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblConsigne
@@ -52,6 +56,7 @@
             this.txtEntry.Name = "txtEntry";
             this.txtEntry.Size = new System.Drawing.Size(210, 33);
             this.txtEntry.TabIndex = 1;
+            this.txtEntry.TextChanged += new System.EventHandler(this.txtEntry_TextChanged);
             // 
             // btnValidate
             // 
@@ -59,24 +64,56 @@
             this.btnValidate.FlatAppearance.BorderSize = 0;
             this.btnValidate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnValidate.ForeColor = System.Drawing.Color.White;
-            this.btnValidate.Location = new System.Drawing.Point(57, 436);
+            this.btnValidate.Location = new System.Drawing.Point(48, 436);
             this.btnValidate.Margin = new System.Windows.Forms.Padding(6);
             this.btnValidate.Name = "btnValidate";
-            this.btnValidate.Size = new System.Drawing.Size(173, 39);
+            this.btnValidate.Size = new System.Drawing.Size(182, 39);
             this.btnValidate.TabIndex = 3;
             this.btnValidate.Text = "Valider";
             this.btnValidate.UseVisualStyleBackColor = false;
+            this.btnValidate.Visible = false;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
             // 
-            // label1
+            // lblError
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(260, 66);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Votre tuteur doit vous donner\r\nson identifiant. Vous devrez le \r\nrentrer ci-desso" +
-    "us.\r\n";
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Tomato;
+            this.lblError.Location = new System.Drawing.Point(43, 35);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(200, 52);
+            this.lblError.TabIndex = 4;
+            this.lblError.Text = "Identifiant invalide.\r\nVeuillez rééssayer.";
+            this.lblError.Visible = false;
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.AutoSize = true;
+            this.lblMenu.ForeColor = System.Drawing.Color.White;
+            this.lblMenu.Location = new System.Drawing.Point(104, 35);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(67, 26);
+            this.lblMenu.TabIndex = 5;
+            this.lblMenu.Text = "Menu";
+            this.lblMenu.Visible = false;
+            // 
+            // lblTracks
+            // 
+            this.lblTracks.AutoSize = true;
+            this.lblTracks.ForeColor = System.Drawing.Color.White;
+            this.lblTracks.Location = new System.Drawing.Point(52, 109);
+            this.lblTracks.Name = "lblTracks";
+            this.lblTracks.Size = new System.Drawing.Size(180, 26);
+            this.lblTracks.TabIndex = 6;
+            this.lblTracks.Text = "Traceurs actifs : 3";
+            this.lblTracks.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // frmHandicap
             // 
@@ -84,7 +121,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(281, 589);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTracks);
+            this.Controls.Add(this.lblMenu);
+            this.Controls.Add(this.lblError);
             this.Controls.Add(this.btnValidate);
             this.Controls.Add(this.txtEntry);
             this.Controls.Add(this.lblConsigne);
@@ -95,6 +135,7 @@
             this.Name = "frmHandicap";
             this.Text = "VigilHome";
             this.Load += new System.EventHandler(this.frmHandicap_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,7 +146,10 @@
         private System.Windows.Forms.Label lblConsigne;
         private System.Windows.Forms.TextBox txtEntry;
         private RJButton btnValidate;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblMenu;
+        private System.Windows.Forms.Label lblTracks;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- vigilhome/Form1.cs
+++ vigilhome/Form1.cs
@@ -17,11 +17,51 @@ namespace vigilhome
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Charge le programme et change la couleur de fond
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmHandicap_Load(object sender, EventArgs e)
         {
-            if (txtEntry.Text == null)
+            BackColor = ColorTranslator.FromHtml("#433861");
+        }
+        /// <summary>
+        /// Vérifie validité des identifiants
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            // Cache tous les éléments
+            if (txtEntry.Text.Contains("AS"))
             {
-                btnValidate.Enabled = false;
+                lblError.Hide();
+                lblConsigne.Hide();
+                btnValidate.Hide();
+                txtEntry.Hide();
+                lblMenu.Show();
+                lblTracks.Show();
+            }
+            else
+            {
+                lblError.Show();
+            }
+        }
+        /// <summary>
+        /// vérifie que la personne entre un id correct
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtEntry_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEntry.Text.Length > 0)
+            {
+                btnValidate.Show();
+            }
+            if (txtEntry.Text.Length == 0)
+            {
+                btnValidate.Hide();
             }
         }
     }
